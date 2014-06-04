fb9ae11f5634883d225e15edc1e68ef0224abb9b
Christian Otkjær <christian.otkjaer@gmail.com>
2014-06-04 15:15:44 +0200

    Release 0.0.2

---
+++ Classes/NSArray+Map.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Map.h
+//  Pods
+//
+//  Created by Christian Otkjær on 04/06/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Map)
+
+@end
---
+++ Classes/NSArray+Map.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+Map.m
+//  Pods
+//
+//  Created by Christian Otkjær on 04/06/14.
+//
+//
+
+#import "NSArray+Map.h"
+
+@implementation NSArray (Map)
+
+@end
