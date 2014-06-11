5366f62f5a2d069714c25f58a0818b3b456a71c0
Christian Otkjær <christian.otkjaer@gmail.com>
2014-06-11 14:53:30 +0200

    Release 0.0.4

---
+++ NSArray+Random.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Random.h
+//  Silverback
+//
+//  Created by Christian Otkjær on 9/21/13.
+//  Copyright (c) 2013 Silverback IT. All rights reserved.
+//
+
+@interface NSArray (Random)
+
+- (id)randomObject;
+
+@end
